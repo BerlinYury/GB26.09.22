6652ca3947eb7771b83d2f0fc7653d8027b2f59f
BerlinYury <Berlin.yuriy2013@gmail.com>
2023-01-18 18:26:09 +0200

    hw4

---
+++ src/Java1/lesson5/hw5/App.java
@@ -0,0 +1,21 @@
+package Java1.lesson5.hw5;
+
+import java.util.ArrayList;
+
+public class App {
+    public static void main(String[] args) {
+//        ArrayList<Stuff> stuff =new ArrayList<>();
+//        stuff.add(new Stuff("Berkutov Andrey", "manager","456@mail.ru","89456783421",650000,20));
+//        stuff.add(new Stuff("Berlin Yuriy", "secretary","456@mail.ru","89456783421",50000,30));
+//        stuff.add(new Stuff("Aksenov Vladislav", "engineer","456@mail.ru","89456783421",70000,45));
+//        stuff.add(new Stuff("Karpov Anatoliy", "director","456@mail.ru","89456783421",130000,60));
+//        stuff.add(new Stuff("Simonova Tatyana", "secretary","456@mail.ru","89456783421",30000,18));
+//        stuff.add(new Stuff("Kozak Olga", "engineer","456@mail.ru","89456783421",70000,55));
+//        stuff.add(new Stuff("Reshetnikova Viktoriya", "manager","456@mail.ru","89456783421",60000,60));
+//
+//        for (Stuff value : stuff) {
+//            if (value.getAge() > 40)
+//                System.out.println(value);
+//        }
+    }
+}
